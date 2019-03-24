bc3b9e78e8e5cd47cfb41a359d8d6242d2ea7244
White Fossa <fossa@fossa-main.yerrkhao-highspeed.whitefossa.org>
2019-03-24 15:16:00 +0300

    - Updated Readme
    - Removed non-necessary code from l2hal_custom

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# stm32-l2hal
-Level 2 HAL (over STM's one) for STM32 MCUs
+# stm32-l2hal - STM32 MCU level 2 HAL
+
+Open-source (GPLv3+) extension (second level HAL, L2HAL) over STM's HAL for STM32 MCUs.
+
+STM's HAL is designed to provide abstractions over operations like sending data over I2C bus or changing GPIOs states.
+
+My own HAL is designed to provide second level of abstraction like "turn on pixel on display, connected over I2C" or "get incremental encoder turn angle". Actually it's just a set of drivers for various hardware.
+
+Note, that you need my https://github.com/WhiteFossa/stm32-fmgl/ library in your project too, because display drivers included in L2HAL depends on it.
+
+## Features
+- Well-written code (I hope). I've tried to implement everything in understandable, enterprise-style, sacrifice performance for sake of portability and clarity.
+- Each function is documented.
+- GPLv3 license.
--- l2hal/include/l2hal_custom.h
+++ l2hal/include/l2hal_custom.h
@@ -42,53 +42,9 @@
 #ifndef L2HAL_INCLUDE_L2HAL_CUSTOM_H_
 #define L2HAL_INCLUDE_L2HAL_CUSTOM_H_
 
-#include <l2hal_ssd1306.h>
-
-/**
- * I2C1 interrupt priorities.
- */
-#define I2C1_ER_IRQN_PRIORITY 0
-#define I2C1_ER_IRQN_SUBPRIORITY 1
-
-#define I2C1_EV_IRQN_PRIORITY 0
-#define I2C1_EV_IRQN_SUBPRIORITY 2
-
-/**
- * I2C bus handle.
- */
-I2C_HandleTypeDef I2CHandle;
-
-/**
- * Display driver context.
- */
-L2HAL_SSD1306_ContextStruct L2HAL_SSD1306_Context;
-
 /**
  * Put custom hardware initialization stuff here,
  */
 void L2HAL_InitCustomHardware(void);
-
-/**
- * Settig up I2C for display.
- */
-void L2HAL_SetupI2C(void);
-
-/**
- * I2C init.
- */
-void HAL_I2C_MspInit(I2C_HandleTypeDef *hi2c);
-
-/**
- * I2C shutdown.
- */
-void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c);
-
-/**
- * I2C interrput handlers.
- */
-void I2C1_EV_IRQHandler(void);
-void I2C1_ER_IRQHandler(void);
-void HAL_I2C_MemTxCpltCallback(I2C_HandleTypeDef *hi2c);
-
 
 #endif /* L2HAL_INCLUDE_L2HAL_CUSTOM_H_ */
