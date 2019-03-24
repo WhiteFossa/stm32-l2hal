f30ebc318505f4f3ac311594c70da38042459707
White Fossa <fossa@fossa-main.yerrkhao-highspeed.whitefossa.org>
2019-03-24 15:39:04 +0300

    Fixed GPL headers

--- l2hal/drivers/display/ssd1306/include/l2hal_ssd1306_private.h
+++ l2hal/drivers/display/ssd1306/include/l2hal_ssd1306_private.h
@@ -7,7 +7,7 @@
 	Foundation, either version 3 of the License, or (at your option)
 	any later version.
 
-	Power Unit Cooler Controller firmware is distributed in the hope
+	STM32 level 2 HAL is distributed in the hope
 	that it will be useful, but WITHOUT ANY WARRANTY; without even
 	the implied warranty of MERCHANTABILITY or FITNESS FOR A
 	PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -32,7 +32,7 @@
 	or as last measure try to search for #WhiteFossa hashtag.
 
 	-------------------------------------------------------------------------
- */
+//  */
 
 /**
  * @file
