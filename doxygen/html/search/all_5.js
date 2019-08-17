84296d5a6a201410a8b2deaa6bb9fd246c2c6d57
White Fossa <fossa@fossa-main.yerrkhao-highspeed.whitefossa.org>
2019-08-17 21:56:49 +0300

    - Updated documentation
    - Fixed accidentally duplicated drivers directory

--- doxygen/html/search/all_5.js
+++ doxygen/html/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['i2chandle',['I2CHandle',['../struct_l2_h_a_l___s_s_d1306___context_struct.html#a0a178f1a5f6cd5df612766ea8e1ff612',1,'L2HAL_SSD1306_ContextStruct']]],
+  ['i2chandle',['I2CHandle',['../struct_l2_h_a_l___s_s_d1306___context_struct.html#aa7437bb9c80e1a66d1f6bd4095cc3676',1,'L2HAL_SSD1306_ContextStruct']]],
   ['isdatatransferinprogress',['IsDataTransferInProgress',['../struct_l2_h_a_l___s_s_d1306___context_struct.html#ad42925d8e63b940bbdf2158123933436',1,'L2HAL_SSD1306_ContextStruct']]],
   ['isfound',['IsFound',['../struct_l2_h_a_l___s_s_d1306___context_struct.html#acb060626c427e9deb521a16efcd37def',1,'L2HAL_SSD1306_ContextStruct']]],
   ['isportdatacanbeupdated',['IsPortDataCanBeUpdated',['../struct_l2_h_a_l___buttons___context_struct.html#a63f1a3f52293a3cafeb6ee76546e980b',1,'L2HAL_Buttons_ContextStruct']]]
--- doxygen/html/search/variables_3.js
+++ doxygen/html/search/variables_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['framebuffer',['Framebuffer',['../struct_l2_h_a_l___s_s_d1306___context_struct.html#acec794b021e96d064fbdc569745e25d8',1,'L2HAL_SSD1306_ContextStruct']]]
+  ['framebuffer',['Framebuffer',['../struct_l2_h_a_l___s_s_d1306___context_struct.html#ad058c7865da58f61afe6854aaededdde',1,'L2HAL_SSD1306_ContextStruct']]]
 ];
